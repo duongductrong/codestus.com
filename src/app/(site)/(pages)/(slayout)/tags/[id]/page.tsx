b5f549e01af4a1fcdab59e6e0567de485bd968cf
Dương Đức Trọng <duongductrong06@gmail.com>
2023-10-11 23:52:44 +0700

    feat: Add open-graph and metadata for tags page

--- src/app/(site)/(pages)/(slayout)/tags/[id]/page.tsx
+++ src/app/(site)/(pages)/(slayout)/tags/[id]/page.tsx
@@ -4,9 +4,49 @@ import { PAGE_URLS } from "@/constants/urls"
 import postService from "@/services/post-service"
 import tagService from "@/services/tag-service"
 import { ParamsProps } from "@/types/utilities"
+import { Metadata } from "next"
 import { notFound } from "next/navigation"
+import OpenGraphImage from "@/assets/images/open-graph-image.png"
 
 export interface PostsTagProps extends ParamsProps<"id"> {}
+
+export async function generateMetadata({ params: { id } }: PostsTagProps): Promise<Metadata> {
+  const tag = await tagService.detail(id)
+
+  if (!tag) return notFound()
+
+  const canonical = PAGE_URLS.TAG_DETAIL.replace(":id", tag?.tagId.toString())
+
+  return {
+    title: tag?.name,
+    description: tag?.description,
+    alternates: {
+      canonical,
+    },
+    openGraph: {
+      url: canonical,
+      type: "article",
+      title: tag?.name || "",
+      description: tag?.description || "",
+      countryName: "Viet Nam",
+      locale: "vi",
+      siteName: "Codestus.com",
+      publishedTime: tag?.created_at?.toDateString(),
+      emails: ["duongductrong06@gmail.com"],
+      modifiedTime: tag?.updated_at?.toDateString(),
+      authors: ["Duong Duc Trong"],
+      images: [OpenGraphImage.src],
+    },
+    twitter: {
+      title: tag?.name || "",
+      description: tag?.description || "",
+      card: "summary_large_image",
+      creatorId: "duongductrong_",
+      creator: "Duong Duc Trong",
+      images: [OpenGraphImage.src],
+    },
+  }
+}
 
 const PostsTag = async ({ params: { id } }: PostsTagProps) => {
   const tag = await tagService.detail(id)
