96523d516675a68a98ff774778091a48db15737c
Dương Đức Trọng <duongductrong06@gmail.com>
2023-10-22 14:51:19 +0700

    fix: test vercel dir (n+1)

--- src/app/api/dir/route.ts
+++ src/app/api/dir/route.ts
@@ -1,5 +1,9 @@
+import path from "path"
+import fs from "fs"
+
 export function GET(request: Request, response: Response) {
   const q = new URL(request.url)
   const paths = q.searchParams.get("paths") || ""
-  return Response.json({ ok: true, details: __dirname, paths: process.cwd() })
+  const files = fs.readdirSync(path.join(__dirname, paths))
+  return Response.json({ ok: true, dir: __dirname, files, paths })
 }
