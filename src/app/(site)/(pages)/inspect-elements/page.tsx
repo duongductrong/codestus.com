8b1065021db7a8dad79ba29754cd57f7e00f93f9
Dương Đức Trọng <duongductrong06@gmail.com>
2023-10-22 12:49:56 +0700

    remove shiki from inspect-elements page

--- src/app/(site)/(pages)/inspect-elements/page.tsx
+++ src/app/(site)/(pages)/inspect-elements/page.tsx
@@ -3,22 +3,9 @@ import { generateHtmlFromMarkdownVFile, processMarkdown } from "@/lib/markdown"
 import Inspect from "./inspect.mdx"
 
 export default async function InspectElements() {
-  const content = await generateHtmlFromMarkdownVFile(
-    processMarkdown(`
-    # markdown
-    ## markdown
-
-    \`\`\`tsx
-
-    console.log(123)
-
-    \`\`\`
-  `)
-  )
   return (
     <div className="prose dark:prose-invert max-w-full p-4">
       <Inspect />
-      <BlogRenderer content={content} />
     </div>
   )
 }
