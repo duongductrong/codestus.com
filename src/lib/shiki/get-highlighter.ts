e557965cad9f268153ecedc901809daa0b172a39
Dương Đức Trọng <duongductrong06@gmail.com>
2023-10-22 13:48:57 +0700

    fix: test vercel dir (n+1)

--- src/lib/shiki/get-highlighter.ts
+++ src/lib/shiki/get-highlighter.ts
@@ -27,21 +27,25 @@ const touchShikiPath = (): void => {
 }
 
 function fromDir(startPath: string, filter: RegExp, callback: any) {
-  // console.log('Starting from dir '+startPath+'/');
+  try {
+    // console.log('Starting from dir '+startPath+'/');
 
-  if (!fs.existsSync(startPath)) {
-    console.log("no dir ", startPath)
-    return
-  }
+    if (!fs.existsSync(startPath)) {
+      console.log("no dir ", startPath)
+      return
+    }
 
-  const files = fs.readdirSync(startPath)
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < files.length; i++) {
-    const filename = path.join(startPath, files[i])
-    const stat = fs.lstatSync(filename)
-    if (stat.isDirectory()) {
-      fromDir(filename, filter, callback) // recurse
-    } else if (filter.test(filename)) callback(filename)
+    const files = fs.readdirSync(startPath)
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < files.length; i++) {
+      const filename = path.join(startPath, files[i])
+      const stat = fs.lstatSync(filename)
+      if (stat.isDirectory()) {
+        fromDir(filename, filter, callback) // recurse
+      } else if (filter.test(filename)) callback(filename)
+    }
+  } catch {
+    //
   }
 }
 
