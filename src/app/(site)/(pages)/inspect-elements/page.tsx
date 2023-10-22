aa21fd0b1b50fec67ca2744e9d44260845671c3a
Dương Đức Trọng <duongductrong06@gmail.com>
2023-10-22 12:13:10 +0700

    chore: force deploy to fix shiki

--- src/app/(site)/(pages)/inspect-elements/page.tsx
+++ src/app/(site)/(pages)/inspect-elements/page.tsx
@@ -1,9 +1,24 @@
+import BlogRenderer from "@/components/custom-markdowns/blog-renderer"
+import { generateHtmlFromMarkdownVFile, processMarkdown } from "@/lib/markdown"
 import Inspect from "./inspect.mdx"
 
-export default function InspectElements() {
+export default async function InspectElements() {
+  const content = await generateHtmlFromMarkdownVFile(
+    processMarkdown(`
+    # markdown
+    ## markdown
+
+    \`\`\`tsx
+
+    console.log(123)
+
+    \`\`\`
+  `)
+  )
   return (
     <div className="prose dark:prose-invert max-w-full p-4">
       <Inspect />
+      <BlogRenderer content={content} />
     </div>
   )
 }
