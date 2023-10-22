7691a10a4d8fa5127febfc02b6bd66cbd4f99f0b
Dương Đức Trọng <duongductrong06@gmail.com>
2023-10-22 11:59:24 +0700

    chore: Update config for manual deploy vercel

--- next.config.js
+++ next.config.js
@@ -5,11 +5,14 @@ const nextConfig = {
     serverActions: true,
     serverComponentsExternalPackages: ["shiki", "vscode-oniguruma"],
   },
+  env: {
+    APP_URL: process.env.APP_URL,
+    APP_ENV: process.env.APP_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
   images: {
-    domains: [
-      "avatars.githubusercontent.com"
-    ]
-  }
+    domains: ["avatars.githubusercontent.com"],
+  },
 }
 
 const withMDX = require("@next/mdx")()
