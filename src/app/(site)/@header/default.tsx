1f7ba3ae94843a0ded44690d313fe9550a05dc5b
Duong Duc Trong <duongductrong06@gmail.com>
2024-06-02 23:19:22 +0700

    chore: update header

--- src/app/(site)/@header/default.tsx
+++ src/app/(site)/@header/default.tsx
@@ -25,8 +25,15 @@ const SiteHeader = (props: SiteHeaderProps) => {
 
   return (
     <div className="p-5 flex items-center gap-3">
-      <Link href={PAGE_URLS.HOME}>
-        <Heading className="text-2xl font-semibold">Codestus.com</Heading>
+      <Link href={PAGE_URLS.HOME} className="flex items-center gap-2">
+        <Image
+          src="/assets/peeps-avatar-alpha-transparent.png"
+          alt="Peeps Avatar"
+          width={32}
+          height={32}
+          className="w-8 h-w-8 rounded-full mx-auto bg-amber-500"
+        />
+        <Heading className="text-lg font-semibold">Hello, codestus.</Heading>
       </Link>
 
       <ToggleThemeButton className="ml-auto" />
