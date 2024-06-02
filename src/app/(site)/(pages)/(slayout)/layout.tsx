59cf6f4d3ad6c3152f090fbdf90d30be2a2d3f95
Duong Duc Trong <duongductrong06@gmail.com>
2024-06-02 22:43:15 +0700

    chore: update new avatar for homepage

--- src/app/(site)/(pages)/(slayout)/layout.tsx
+++ src/app/(site)/(pages)/(slayout)/layout.tsx
@@ -1,14 +1,20 @@
 import { LayoutProps } from "@/types/utilities"
-import FaceIcon from "./_components/face-icon"
+// import FaceIcon from "./_components/face-icon"
+import Image from "next/image"
 
 export interface SLayoutProps extends LayoutProps<"slayouttags"> {}
 
 const SLayout = ({ slayouttags, children }: SLayoutProps) => (
   <>
-    <FaceIcon className="w-52 h-52 mx-auto" />
-    <div className="px-5 py-4 w-full flex justify-center flex-wrap gap-3">
-      {slayouttags}
-    </div>
+    {/* <FaceIcon className="w-52 h-52 mx-auto" /> */}
+    <Image
+      src="/assets/peeps-avatar-alpha-transparent.png"
+      alt="Peeps Avatar"
+      width={520}
+      height={520}
+      className="w-52 h-w-52 rounded-full mx-auto bg-amber-500"
+    />
+    <div className="px-5 py-4 w-full flex justify-center flex-wrap gap-3">{slayouttags}</div>
     {children}
   </>
 )
