05d03433985ec772f45c9dc94d7c222246c3f216
Duong Duc Trong <duongductrong06@gmail.com>
2024-09-21 09:52:26 +0700

    chore: resolve error build

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,18 @@
+import { allTags } from "@/constants/tags"
 import { PAGE_URLS } from "@/constants/urls"
 import { prisma } from "@/lib/prisma"
+import { allPosts } from "content-collections"
 import dayjs from "dayjs"
 import { MetadataRoute } from "next"
 
 const URL = process.env.APP_URL
 
 export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
-  const [posts, tags] = await prisma.$transaction([
-    prisma.post.findMany({}),
-    prisma.tag.findMany({}),
-  ])
+  // const [posts, tags] = await prisma.$transaction([
+  //   prisma.post.findMany({}),
+  //   prisma.tag.findMany({}),
+  // ])
+  const [posts, tags] = [allPosts, allTags]
 
   const staticRoutes = [
     {
@@ -23,7 +26,7 @@ export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
   const postRoutes = posts.map<MetadataRoute.Sitemap[0]>((post) => ({
     url: `${URL}${PAGE_URLS.POST_DETAIL.replace(":id", post.slug)}`,
     changeFrequency: "daily",
-    lastModified: dayjs(post.publish_at).toDate(),
+    lastModified: dayjs(post.publishAt).toDate(),
     priority: 1,
   }))
 
