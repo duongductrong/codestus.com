a248ac2ff38e7ab8eac44413fc317a4b05051b92
Duong Duc Trong <duongductrong06@gmail.com>
2025-04-20 15:42:20 +0700

    chore(db): update PostgreSQL connection configuration to disable query preparation

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,9 @@ import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 import * as schema from "./schema"
 
-const sql = postgres(process.env.DATABASE_URL!)
+const sql = postgres(process.env.DATABASE_URL!, {
+  prepare: false,
+})
 
 export const db = drizzle(sql, {
   schema,
