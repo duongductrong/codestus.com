2dd63b556a4ea60f751423887f8ad0b2628b8f06
Duong Duc Trong <duongductrong06@gmail.com>
2025-04-20 15:30:30 +0700

    chore: removed logger and debugger on db/ setting

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,10 +4,8 @@ import * as schema from "./schema"
 
 const sql = postgres(process.env.DATABASE_URL!, {
   max: 1,
-  debug: process.env.NODE_ENV === "development",
 })
 
 export const db = drizzle(sql, {
   schema,
-  logger: process.env.NODE_ENV === "development",
 })
