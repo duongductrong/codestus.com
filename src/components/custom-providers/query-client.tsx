ddfdc8892499dc0c3961a07c5825de0492af87b8
Duong Duc Trong <duongductrong06@gmail.com>
2025-06-08 14:31:11 +0700

    chore: adjust options of query client

--- src/components/custom-providers/query-client.tsx
+++ src/components/custom-providers/query-client.tsx
@@ -6,7 +6,13 @@ import {
 } from "@tanstack/react-query"
 import { PropsWithChildren } from "react"
 
-const queryClient = new QueryClient({})
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+})
 
 export const QueryClientProvider = ({ children }: PropsWithChildren) => {
   return <QueryClientProviderTanstack client={queryClient}>{children}</QueryClientProviderTanstack>
