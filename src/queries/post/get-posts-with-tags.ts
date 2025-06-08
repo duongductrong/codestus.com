ed4cd8f0bbfdb2a2d7bdcb04cf6dea79fc841e4c
Duong Duc Trong <duongductrong06@gmail.com>
2025-06-08 15:21:46 +0700

    feat(post): filter out draft posts in getPostsWithTags query to improve post retrieval

--- src/queries/post/get-posts-with-tags.ts
+++ src/queries/post/get-posts-with-tags.ts
@@ -1,7 +1,9 @@
 import { db } from "@/db"
+import { eq, not } from "drizzle-orm"
 
 export const getPostsWithTags = async () => {
   const posts = await db.query.postTable.findMany({
+    where: (post) => not(eq(post.status, "draft")),
     with: {
       tags: {
         with: {
